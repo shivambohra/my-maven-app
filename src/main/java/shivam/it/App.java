f6c2d797e6bf4e7b0011c9f1c6a145aea9518330
shiva <shivambohra004@gmail.com>
2023-08-27 17:30:02 +0530

    my first commmit

---
+++ src/main/java/shivam/it/App.java
@@ -0,0 +1,13 @@
+package shivam.it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello to the World of java!" );
+    }
+}
